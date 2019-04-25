7140904bb7c711915df3233d3f6c2c89de1a8ebb
Kalyan Sikdar <skbrata@gmail.com>
2019-04-24 21:38:15 -0500

    added put, delete methods.

--- tutorial-course-api/src/main/java/io/kalyansikdar/springbootstarter/Topic/TopicService.java
+++ tutorial-course-api/src/main/java/io/kalyansikdar/springbootstarter/Topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.kalyansikdar.springbootstarter.Topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,12 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("Java Spring Boot", "JSB", "Java Spring Boot Tutorial"),
 			new Topic("Front-End Development", "FED", "Udemy Front-End Tutorial"),
 			new Topic("Python Django", "PD", "Youtube Python Django Tutorial"),
 			new Topic("AWS Tutorial", "AWS", "AWS Tutorial Certification")
-			);
+			));
 	
 	List<Topic> getAllTopics(){
 		return this.topics;
@@ -26,5 +27,23 @@ public class TopicService {
 			}
 		}
 		return null;
+//		return topics.stream().filter(t-> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopics(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopics(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++) {
+			if (topics.get(i).getId().equals(id)) {
+				topics.set(i, topic);
+				return; // whenever the item is replaces, function returns
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(topic -> topic.getId().equals(id));
 	}
 }
